35d37d92ed62753a7161a10b730cfe3df5fe9aa2
McGavInvincible <pdsmcgavin@gmail.com>
2017-09-04 13:05:35 +0100

    Now writing matches to database

--- lib/database_scraper.ex
+++ lib/database_scraper.ex
@@ -1,5 +1,7 @@
 defmodule DatabaseScraper do
 
+  alias WweloTest.Stats
+
   def list_of_matches do
     response=HTTPoison.get!("https://www.cagematch.net/?id=112&view=search&sEventType=TV-Show|Pay%20Per%20View&sDateFromDay=01&sDateFromMonth=01&sDateFromYear=2017&sDateTillDay=31&sDateTillMonth=12&sDateTillYear=2017&sPromotion=1&sWorkerRelationship=Any&s=0")
 
@@ -17,15 +19,6 @@ defmodule DatabaseScraper do
       |> Enum.filter(fn(y) -> is_tuple(y) end)
       |> Enum.filter(fn(z) -> elem(z, 0) == "a" end)
       |> length == 2 end)
-
-    dates = Enum.map(singles_table_contents, fn(x) -> Enum.at(x, 1) |> elem(2) |> Enum.at(0) end)
-
-    matchcards = Enum.map(singles_table_contents, fn(x) -> Floki.find(x, ".MatchCard") end)
-
-    winners = Enum.map(matchcards, fn(x) -> Enum.at(x, 0) |> elem(2) |> Enum.at(0) |> elem(2) |> Enum.at(0) end)
-    losers = Enum.map(matchcards, fn(x) -> Enum.at(x, 0) |> elem(2) |> Enum.at(2) |> elem(2) |> Enum.at(0) end)
-
-    singles_table_contents
    end
 
    def save_matches_to_database([_, {_, _, [date]}, _, matchcard, _]) do
@@ -34,7 +27,7 @@ defmodule DatabaseScraper do
     {_, _, [winner]} = Enum.at(matchcard, 0)
     {_, _, [loser]} = Enum.at(matchcard, 2)
 
-    {winner, loser, date}
+    Stats.create_matches(%{"winner" => winner, "winner_elo" => 0, "loser" => loser, "loser_elo" => 0, "date" => date})
    end
 
 end
