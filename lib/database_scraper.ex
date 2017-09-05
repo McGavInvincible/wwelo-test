be87808fd996c8ba564c9774418b47c8c9ef6e08
McGavInvincible <pdsmcgavin@gmail.com>
2017-09-05 13:34:39 +0100

    Cleaning up databasescraper

--- lib/database_scraper.ex
+++ lib/database_scraper.ex
@@ -2,32 +2,44 @@ defmodule DatabaseScraper do
 
   alias WweloTest.Stats
 
-  def list_of_matches do
-    response=HTTPoison.get!("https://www.cagematch.net/?id=112&view=search&sEventType=TV-Show|Pay%20Per%20View&sDateFromDay=01&sDateFromMonth=01&sDateFromYear=2017&sDateTillDay=31&sDateTillMonth=12&sDateTillYear=2017&sPromotion=1&sWorkerRelationship=Any&s=0")
+  def save_singles_matches do
+    Enum.map(list_of_singles_matches(), fn(x) -> save_matches_to_database(x) end)
+  end
 
-    table_contents = Floki.parse(response.body)
-      |> Floki.find(".TBase")
-      |> Enum.at(0)
-      |> elem(2)
-      |> Enum.map(fn(x) -> elem(x, 2) end)
+  def list_of_singles_matches do
+    table_contents = get_table_contents("https://www.cagematch.net/?id=112&view=search&sEventType=TV-Show|Pay%20Per%20View&sDateFromDay=01&sDateFromMonth=01&sDateFromYear=2017&sDateTillDay=31&sDateTillMonth=12&sDateTillYear=2017&sPromotion=1&sWorkerRelationship=Any&s=0")
 
-    singles_table_contents = tl(table_contents)
-      |> Enum.filter(fn(x) -> Enum.at(x, 3)
-      |> Floki.find(".MatchCard")
-      |> Enum.at(0)
-      |> elem(2)
-      |> Enum.filter(fn(y) -> is_tuple(y) end)
-      |> Enum.filter(fn(z) -> elem(z, 0) == "a" end)
-      |> length == 2 end)
+    get_singles_matches_from_table(table_contents)
+   end
+
+   def get_table_contents(link) do
+     response=HTTPoison.get!(link)
+
+     Floki.parse(response.body)
+       |> Floki.find(".TBase")
+       |> Enum.at(0)
+       |> elem(2)
+       |> Enum.map(fn(x) -> elem(x, 2) end)
+   end
+
+   def get_singles_matches_from_table(table_contents) do
+     tl(table_contents)
+       |> Enum.filter(fn(x) -> Enum.at(x, 3)
+       |> Floki.find(".MatchCard")
+       |> Enum.at(0)
+       |> elem(2)
+       |> Enum.filter(fn(y) -> is_tuple(y) end)
+       |> Enum.filter(fn(z) -> elem(z, 0) == "a" end)
+       |> length == 2 end)
    end
 
    def save_matches_to_database([_, {_, _, [date]}, _, matchcard, _]) do
      matchcard = Floki.find(matchcard, ".MatchCard") |> Enum.at(0) |> elem(2)
 
-    {_, _, [winner]} = Enum.at(matchcard, 0)
-    {_, _, [loser]} = Enum.at(matchcard, 2)
+      {_, _, [winner]} = Enum.at(matchcard, 0)
+      {_, _, [loser]} = Enum.at(matchcard, 2)
 
-    Stats.create_matches(%{"winner" => winner, "winner_elo" => 0, "loser" => loser, "loser_elo" => 0, "date" => date})
+      Stats.create_matches(%{"winner" => winner, "winner_elo" => 0, "loser" => loser, "loser_elo" => 0, "date" => date})
    end
 
 end
