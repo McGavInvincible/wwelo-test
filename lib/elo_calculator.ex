5d22a0af71a380b1f5a002517bbadc86fe9dca40
McGavInvincible <pdsmcgavin@gmail.com>
2017-09-07 17:14:31 +0100

    List of matches for elo calculation

--- lib/elo_calculator.ex
+++ lib/elo_calculator.ex
@@ -1,5 +1,10 @@
 defmodule EloCalculator do
 
+  import Ecto.Query
+
+  alias WweloTest.Repo
+  alias WweloTest.Stats.Matches
+
   def elo_calculator(winner_elo, loser_elo) do
     rwinner = Math.pow(10,(winner_elo/400))
     rloser = Math.pow(10,(loser_elo/400))
@@ -13,6 +18,15 @@ defmodule EloCalculator do
     updated_loser_elo = loser_elo + match_weight*(-eloser)
 
     %{winner_elo: Float.round(updated_winner_elo), loser_elo: Float.round(updated_loser_elo)}
+  end
+
+  def list_of_ids_of_new_matches do
+    new_matches_query = from m in Matches,
+      where: is_nil(m.winner_elo) and is_nil(m.loser_elo),
+      order_by: [asc: m.date],
+      select: m.id
+
+    Repo.all(new_matches_query)
   end
 
 end
